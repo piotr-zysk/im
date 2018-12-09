d9565ffe2e2472b52290a03c62e5acd3d7733654
Piotr Zysk <pizy123@gmail.com>
2018-12-09 21:30:38 +0100

    starting with listbox - https://www.youtube.com/watch?v=BTwfyfv1v84

--- src/components/RecipientPickup.vue
+++ src/components/RecipientPickup.vue
@@ -1,9 +1,29 @@
 <template>
+<div>
 <p>wybór adresatów</p>
+<!-- https://www.youtube.com/watch?v=BTwfyfv1v84 -->
+
+<select class="listbox" id="list1" multiple>
+    <option v-for="item1 in list1" :key="item1">{{item1}}</option>
+</select>
+</div>
 </template>
 
 <script>
 export default {
-  name: "NewMessage"
+  name: "NewMessage",
+  data() { return {
+      list1: ['adedede','b','c','d']
+  }}
 }
+
 </script>
+
+<style>
+.listbox {
+    width: 250px;
+    height: 200px;
+    display: block;
+}
+
+</style>
